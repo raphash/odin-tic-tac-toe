ce8d26852ae97c3b451a6a6ef16b369b90f93e3b
raphash <175216103+raphash@users.noreply.github.com>
2024-12-27 20:30:16 -0300

    feat: Add Gameboard IIFE to control board flow
    The board is responsible for inserting pieces onto
    the board, resetting it to its initial state,
    returning if there is free space, etc.

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello, World!");
+// Object responsible for manipulating the game board
+const Gameboard = (function(){
+  
+  let board = [];
+
+  function getBoard() {
+    return board;
+  }
+
+  function isSpaceAvaiable(row, col) {
+    return !board[row][col];
+  }
+  
+  function setBoardPiece(row, col, marker) {
+    if (isSpaceAvaiable(row, col)) {
+      board[row][col] = marker;
+    }
+  }
+
+  // Sets the board to it initial state.
+  function resetBoard() {
+    board.splice(0, board.length);
+
+    for (let row = 0; row < 3; row++) {
+      board.push([]);
+      
+      for (let col = 0; col < 3; col++) {
+        board[row].push("");
+      }
+    }
+  }
+
+  return {
+    getBoard,
+    isSpaceAvaiable,
+    setBoardPiece,
+    resetBoard
+  }
+
+})();
