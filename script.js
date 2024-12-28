8673bdb77707a6f1ae1a6b81535eab19958d5b61
raphash <175216103+raphash@users.noreply.github.com>
2024-12-27 21:55:51 -0300

    feat: GameController IIFE to control the game flow
    GameController can play a round, check if have a winner,
    check if the game its a draw, return the current player, etc.
    This have methods that is resposible for game flow.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Object responsible for manipulating the game board
+// Object responsible for manipulating the game board.
 const Gameboard = (function(){
   
   let board = [];
@@ -39,7 +39,7 @@ const Gameboard = (function(){
 
 })();
 
-// Object resposible by manipulation the players
+// Object resposible by manipulating the players.
 const PlayerManager = (function(){
   
   const players = [];
@@ -55,5 +55,100 @@ const PlayerManager = (function(){
   return {
     getPlayers,
     createPlayer
+  }
+})();
+
+// Object resposible by manipulating the game flow.
+const GameController = (function(){
+
+  // Set board to it initial state.
+  Gameboard.resetBoard();
+
+  // Create the two players of the game;
+  PlayerManager.createPlayer("x");
+  PlayerManager.createPlayer("o");
+
+  let players = PlayerManager.getPlayers();
+  let currentPlayer = players[0];
+
+  function getWinner(marker) {
+    const board = Gameboard.getBoard();
+
+    // Check if marker fills an row.
+    for (let row = 0; row < board.length; row++) {
+      if (board[row][0] == marker &&
+          board[row][1] == marker &&
+          board[row][2] == marker) {
+            return true;
+      }
+    }
+
+    // Check if marker fills an column. 
+    for (let col = 0; col < board.length; col++) {
+      if (board[0][col] == marker &&
+          board[1][col] == marker &&
+          board[2][col] == marker) {
+            return true;
+      }
+    }
+
+    // Check if marker fills an diagonal.
+    if (board[0][0] == marker &&
+        board[1][1] == marker &&
+        board[2][2] == marker) {
+          return true;
+    }
+
+    if (board[0][2] == marker &&
+        board[1][1] == marker &&
+        board[2][0] == marker) {
+          return true;
+    }
+
+    return false;
+  }
+
+  function getDraw() {
+    for (const row of Gameboard.getBoard()) {
+      for (col of row) {
+        if (!col) return false;
+      }
+    }
+
+    return true;
+  }
+
+  function getCurrentPlayer() {
+    return currentPlayer;
+  }
+
+  function switchPlayer() {
+    currentPlayer = currentPlayer == players[0] ? players[1] : players[0];
+  }
+
+  function playRound(row, col, marker) {
+    if (Gameboard.isCellAvaiable(row, col)) {
+      Gameboard.setBoardPiece(row, col, marker);
+
+      if (getWinner(marker)) {
+        console.log(`${marker} is the winner!`);
+        Gameboard.resetBoard();
+      }
+
+      if (getDraw()) {
+        console.log(`its a draw!`);
+        Gameboard.resetBoard();
+      };
+
+      switchPlayer();
+    }
+  }
+
+  return {
+    getCurrentPlayer,
+    switchPlayer,
+    playRound,
+    getWinner,
+    getDraw,
   }
 })();
